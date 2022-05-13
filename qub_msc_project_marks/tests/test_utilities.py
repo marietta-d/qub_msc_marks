ef29780ac857c28408b2f9db0e3c10884e36b4cf
Marietta Dalamanga <m.dalamagga@gmail.com>
2022-05-13 20:05:48 +0100

    test for utilities

--- qub_msc_project_marks/tests/test_utilities.py
+++ qub_msc_project_marks/tests/test_utilities.py
@@ -1,8 +1,22 @@
 import unittest
+import roasted_roster.utilities as util
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+class UtilitiesTestCase(unittest.TestCase):
+    def test_find_in_list(self):
+        my_list = ["abc", "def", "ghi"]
+        keyword = "ghi"
+        actual_loc = util.find_in_list(my_list, keyword)
+        expected_loc = 2
+        self.assertEqual(expected_loc, actual_loc)
+
+    def test_find_in_list_not_there(self):
+        my_list = ["abc", "def", "ghi"]
+        keyword = "h"
+        actual_loc = util.find_in_list(my_list, keyword)
+        expected_loc = None
+        self.assertEqual(expected_loc, actual_loc)
+
 
 if __name__ == '__main__':
     unittest.main()
