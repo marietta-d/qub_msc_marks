9a4daf3d7c741814b6d064c7a2898c61dd273819
Marietta Dalamanga <m.dalamagga@gmail.com>
2022-05-13 20:04:52 +0100

    tests for prog_board

--- qub_msc_project_marks/tests/test_prog_board.py
+++ qub_msc_project_marks/tests/test_prog_board.py
@@ -1,8 +1,22 @@
 import unittest
+import roasted_roster.prog_board as pb
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+class ProgBoardTestCase(unittest.TestCase):
+    def test_load_subject_list(self):
+        board_file_name = "../data/subject_data/programme_board_template (target file blank).xlsx"
+        y = pb.load_subject_list(board_file_name)
+        print(y)
+
+    def test_copy_from_marksheet_to_prog_board(self):
+        marksheet_file_name = "../data/subject_data/subjects/ele8066.xlsx"
+        pb_filename = "../data/subject_data/programme_board_template (target file blank).xlsx"
+        subjects = pb.load_subject_list(pb_filename)
+        bookkeeping_list = pb.copy_from_marksheet_to_prog_board(marksheet_file_name, pb_filename, [], subjects)
+        print(bookkeeping_list)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
