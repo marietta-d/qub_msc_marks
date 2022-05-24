1e58dcd462ac2e4dd74f49f6ae8e26397ce04225
Marietta Dalamanga <m.dalamagga@gmail.com>
2022-05-24 18:28:25 +0100

    add test for fill_prog_board

--- qub_msc_project_marks/tests/test_prog_board.py
+++ qub_msc_project_marks/tests/test_prog_board.py
@@ -9,11 +9,17 @@ class ProgBoardTestCase(unittest.TestCase):
         print(y)
 
     def test_copy_from_marksheet_to_prog_board(self):
-        marksheet_file_name = "../data/subject_data/subjects/ele8066.xlsx"
+        marksheet_file_name = "../data/subject_data/subjects/ele8078.xlsx"
         pb_filename = "../data/subject_data/programme_board_template (target file blank).xlsx"
         subjects = pb.load_subject_list(pb_filename)
         bookkeeping_list = pb.copy_from_marksheet_to_prog_board(marksheet_file_name, pb_filename, [], subjects)
         print(bookkeeping_list)
+
+    def test_fill_prog_board(self):
+        prog_board_file_name = "../data/subject_data/programme_board_template (target file blank).xlsx"
+        subject_folder = "../data/subject_data/subjects"
+        final_file = pb.fill_prog_board(prog_board_file_name, subject_folder)
+        print(final_file)
 
 
 
